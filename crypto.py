8feb3a24dda7bbd73e7e3ff774341fdfefdc6310
valescaalvesc <valescaalvess@gmail.com>
2022-07-25 20:01:33 -0300

    Primeira atualização, iniciando biblioteca

---
+++ crypto.py
@@ -0,0 +1,6 @@
+from sqlite3 import paramstyle
+
+
+#fernet é uma ferramenta de criptografia, uma chave que será usada para
+#criptografar e descriptografar uma chave (mensagem)
+from cryptography.fernet import Fernet
